9b16773d1c75c251ffcc3d07f68d508e44daddb5
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2021-02-13 20:47:30 +0300

    feat: add new task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array || array.length === 0)  {
+    return 0;
+  }
+    let posMin = array[0];
+    for (let i = 0; i < array.length; i++) { 
+    if (posMin > array[i]) {
+      posMin = array[i]
+    }
+  }
+  return posMin;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0)  {
+    return 0;
+  }
+  let posMax = array[0];
+  for (let i = 0; i < array.length; i++) { 
+    if (posMax < array[i]) {
+      posMax = array[i]
+    }
+  }
+  return posMax;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0)  {
+    return 0;
+  }
+  return array.reduce((acc, current) => {
+      return acc + current / array.length
+    }) 
 }
